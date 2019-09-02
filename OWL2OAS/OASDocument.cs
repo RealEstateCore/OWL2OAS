ff32897ad31e2253392aeb82d881f938c9ee8c49
Karl Hammar <karl@karlhammar.com>
2019-09-02 20:43:19 +0200

    Initial commit

---
+++ OWL2OAS/OASDocument.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace OWL2OAS
+{
+    class OASDocument
+    {
+        public string openapi { get { return "3.0.0"; } }
+        public Info info { get; set; }
+        public Components components { get; set; }
+        public Paths paths { get; set; }
+
+        public struct Info
+        {
+            public string version;
+            public string title;
+            public License license;
+        }
+
+        public struct License
+        {
+            public string name;
+        }
+
+        public struct Components
+        {
+            public List<Schema> schemas;
+        }
+      
+        public struct Schema
+        {
+            public string title;
+        }
+
+        public struct Paths
+        {
+            public Paths[] paths;
+        }
+    }
+}
---
+++ OWL2OAS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VDS.RDF.Ontology;
+using VDS.RDF.Parsing;
+using YamlDotNet.Serialization;
+
+namespace OWL2OAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load ontology graph
+            OntologyGraph g = new OntologyGraph();
+            FileLoader.Load(g, args[0]);
+
+            // Create OAS object
+            OASDocument document = new OASDocument();
+
+            OASDocument.Info info = new OASDocument.Info();
+            info.title = "RealEstateCore API";
+            info.version = "\"3.1\"";
+            OASDocument.License license = new OASDocument.License();
+            license.name = "MIT";
+            info.license = license;
+            document.info = info;
+
+            OASDocument.Components components = new OASDocument.Components();
+            List<OASDocument.Schema> schemas = new List<OASDocument.Schema>();
+            foreach (OntologyClass c in g.OwlClasses)
+            {
+                OASDocument.Schema schema = new OASDocument.Schema();
+                schema.title = c.ToString();
+                schemas.Add(schema);
+            }
+            components.schemas = schemas;
+            document.components = components;
+
+            DumpAsYaml(document);
+            
+            // Keep window open during debug
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+        }
+
+        private static void DumpAsYaml(object data)
+        {
+            Console.WriteLine("***Dumping Object Using Yaml Serializer***");
+            var stringBuilder = new StringBuilder();
+            var serializer = new Serializer();
+            stringBuilder.AppendLine(serializer.Serialize(data));
+            Console.WriteLine(stringBuilder);
+            Console.WriteLine("");
+        }
+    }
+}
