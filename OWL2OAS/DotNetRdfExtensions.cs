dd4eb12d0961ee2e626f696be4638ccfea47e335
Karl Hammar <karl@karlhammar.com>
2019-11-03 08:56:08 +0100

    Structured extensions better

--- OWL2OAS/DotNetRdfExtensions.cs
+++ OWL2OAS/DotNetRdfExtensions.cs
@@ -13,7 +13,7 @@ namespace OWL2OAS
     /// </summary>
     public static class DotNetRdfExtensions
     {
-
+        #region Shared
         /// <summary>
         /// Custom comparer for OntologyResource objects, that simply
         /// defers to comparison of nested INodes.
@@ -30,112 +30,9 @@ namespace OWL2OAS
                 return obj.Resource.GetHashCode();
             }
         }
+        #endregion
 
-
-        public static bool IsDataProperty(this OntologyProperty property)
-        {
-            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlDatatypeProperty));
-        }
-
-        public static IEnumerable<OntologyProperty> IsScopedDomainOf(this OntologyClass cls)
-        {
-            OntologyGraph graph = cls.Graph as OntologyGraph;
-            IUriNode onProperty = graph.CreateUriNode(new Uri("http://www.w3.org/2002/07/owl#onProperty"));
-            IEnumerable<IUriNode> propertyNodes = cls.SuperClasses.Where(superClass => superClass.IsRestriction())
-                .SelectMany(restriction => restriction.GetNodesViaProperty(onProperty)).UriNodes();
-            return propertyNodes.SelectMany(node => graph.OwlProperties.Where(oProperty => oProperty.Resource.Equals(node)));
-        }
-
-        public static IEnumerable<OntologyProperty> DataProperties(this IEnumerable<OntologyProperty> properties)
-        {
-            return properties.Where(property => property.IsDataProperty());
-        }
-
-        public static IEnumerable<OntologyProperty> ObjectProperties(this IEnumerable<OntologyProperty> properties)
-        {
-            return properties.Where(property => property.IsObjectProperty());
-        }
-
-        public static OntologyGraph OntologyGraph(this OntologyResource resource)
-        {
-            // TODO: Check if this is potentially explosive design
-            return resource.Graph as OntologyGraph;
-        }
-
-        public static IEnumerable<OntologyProperty> IsExhaustiveDomainOf(this OntologyClass oClass)
-        {
-            IEnumerable<OntologyProperty> directDomainProperties = oClass.IsDomainOf;
-            IEnumerable<OntologyProperty> indirectDomainProperties = oClass.SuperClasses.SelectMany(cls => cls.IsDomainOf);
-            IEnumerable<OntologyProperty> scopedDomainProperties = oClass.IsScopedDomainOf();
-            IEnumerable<OntologyProperty> allDomainProperties = directDomainProperties.Union(indirectDomainProperties).Union(scopedDomainProperties);
-            return allDomainProperties.Distinct(new OntologyResourceComparer()).Select(ontResource => ontResource as OntologyProperty);
-        }
-
-        public static bool IsDeprecated(this OntologyResource resource)
-        {
-            IUriNode deprecated = resource.Graph.CreateUriNode(new Uri("http://www.w3.org/2002/07/owl#deprecated"));
-            return resource.GetNodesViaProperty(deprecated).LiteralNodes().Any(node => node.Value == "true");
-        }
-
-        public static bool IsDataProperty(this INode propertyNode)
-        {
-            IGraph graph = propertyNode.Graph;
-            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
-            IUriNode dataProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlDatatypeProperty));
-            return graph.ContainsTriple(new Triple(propertyNode, rdfType, dataProperty));
-        }
-
-        public static bool IsObjectProperty(this OntologyProperty property)
-        {
-            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlObjectProperty));
-        }
-
-        public static bool IsObjectProperty(this INode propertyNode)
-        {
-            IGraph graph = propertyNode.Graph;
-            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
-            IUriNode objectProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlObjectProperty));
-            return graph.ContainsTriple(new Triple(propertyNode, rdfType, objectProperty));
-        }
-
-        public static bool IsAnnotationProperty(this OntologyProperty property)
-        {
-            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlAnnotationProperty));
-        }
-
-        public static bool IsAnnotationProperty(this INode propertyNode)
-        {
-            IGraph graph = propertyNode.Graph;
-            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
-            IUriNode annotationProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlAnnotationProperty));
-            return graph.ContainsTriple(new Triple(propertyNode, rdfType, annotationProperty));
-        }
-
-        public static bool IsOntologyProperty(this INode node)
-        {
-            return node.IsAnnotationProperty() || node.IsDataProperty() || node.IsObjectProperty();
-        }
-
-        public static bool IsRdfsDatatype(this OntologyClass oClass)
-        {
-            return oClass.Types.UriNodes().Any(classType => classType.Uri.Equals(VocabularyHelper.RDFS.Datatype));
-        }
-
-        public static bool IsRestriction(this OntologyClass oClass)
-        {
-            return oClass.Types.UriNodes().Any(classType => classType.Uri.Equals(VocabularyHelper.OWL.Restriction));
-        }
-
-        public static bool IsFunctional(this OntologyProperty property)
-        {
-            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.Equals(VocabularyHelper.OWL.FunctionalProperty));
-        }
-
-        public static bool IsNamed(this OntologyResource ontResource)
-        {
-            return ontResource.Resource.IsUri();
-        }
-
+        #region INode/ILiteralNode/IUriNode extensions
         public static bool IsLiteral(this INode node)
         {
             return node.NodeType.Equals(NodeType.Literal);
@@ -163,18 +60,34 @@ namespace OWL2OAS
                 && (datatype.EndsWith("Integer", StringComparison.Ordinal) || datatype.EndsWith("Int", StringComparison.Ordinal));
         }
 
-        public static IEnumerable<INode> GetNodesViaProperty(this OntologyResource resource, INode property)
+        
+        public static bool IsDataProperty(this INode propertyNode)
         {
-            return resource.Graph.GetTriplesWithSubjectPredicate(resource.Resource, property).Select(triple => triple.Object);
+            IGraph graph = propertyNode.Graph;
+            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+            IUriNode dataProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlDatatypeProperty));
+            return graph.ContainsTriple(new Triple(propertyNode, rdfType, dataProperty));
         }
 
-        public static bool IsXsdDatatype(this OntologyClass oClass)
+        public static bool IsObjectProperty(this INode propertyNode)
         {
-            if (oClass.IsNamed())
-            {
-                return oClass.GetIri().ToString().StartsWith(XmlSpecsHelper.NamespaceXmlSchema, StringComparison.Ordinal);
-            }
-            return false;
+            IGraph graph = propertyNode.Graph;
+            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+            IUriNode objectProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlObjectProperty));
+            return graph.ContainsTriple(new Triple(propertyNode, rdfType, objectProperty));
+        }
+
+        public static bool IsAnnotationProperty(this INode propertyNode)
+        {
+            IGraph graph = propertyNode.Graph;
+            IUriNode rdfType = graph.CreateUriNode(new Uri(RdfSpecsHelper.RdfType));
+            IUriNode annotationProperty = graph.CreateUriNode(new Uri(OntologyHelper.OwlAnnotationProperty));
+            return graph.ContainsTriple(new Triple(propertyNode, rdfType, annotationProperty));
+        }
+
+        public static bool IsOntologyProperty(this INode node)
+        {
+            return node.IsAnnotationProperty() || node.IsDataProperty() || node.IsObjectProperty();
         }
 
         public static string GetLocalName(this IUriNode node)
@@ -195,59 +108,9 @@ namespace OWL2OAS
             string link = node.Uri.GetLeftPart(UriPartial.Path);
             return link.Substring(0, link.LastIndexOf("/", StringComparison.Ordinal) + 1);
         }
+        #endregion
 
-        public static string GetLocalName(this OntologyResource resource)
-        {
-            if (resource.IsNamed())
-            {
-                return ((UriNode)resource.Resource).GetLocalName();
-            }
-            throw new RdfException(string.Format("{0} is not backed by a named URI node.", resource));
-        }
-
-        public static bool HasVersionIri(this Ontology ontology)
-        {
-            IUriNode versionIri = ontology.Graph.CreateUriNode(VocabularyHelper.OWL.versionIRI);
-            return ontology.GetNodesViaProperty(versionIri).UriNodes().Any();
-        }
-
-        public static Uri GetVersionIri(this Ontology ontology)
-        {
-            if (!ontology.HasVersionIri())
-            {
-                throw new RdfException(string.Format("Ontology {0} does not have an owl:versionIRI annotation", ontology));
-            }
-
-            IUriNode versionIri = ontology.Graph.CreateUriNode(VocabularyHelper.OWL.versionIRI);
-            return ontology.GetNodesViaProperty(versionIri).UriNodes().First().Uri;
-        }
-
-        public static Uri GetIri(this OntologyResource resource)
-        {
-            if (!resource.IsNamed())
-            {
-                throw new RdfException(string.Format("Ontology resource {0} does not have an IRI.", resource));
-            }
-
-            return ((UriNode)resource.Resource).Uri;
-        }
-
-        /// <summary>
-        /// Gets the version IRI of the ontology, if it is defined, or the ontology
-        /// IRI if it is not. If neither is defined (i.e. the ontology is anonymous),
-        /// throws an exception.
-        /// </summary>
-        /// <param name="ontology"></param>
-        /// <returns></returns>
-        public static Uri GetVersionOrOntologyIri(this Ontology ontology)
-        {
-            if (ontology.HasVersionIri())
-            {
-                return ontology.GetVersionIri();
-            }
-            return ontology.GetIri();
-        }
-
+        #region OntologyGraph extensions
         public static bool HasBaseUriOntology(this OntologyGraph graph)
         {
             IUriNode baseUriNode = graph.CreateUriNode(graph.BaseUri);
@@ -298,6 +161,85 @@ namespace OWL2OAS
             }
             throw new RdfException(string.Format("The graph {0} doesn't contain any owl:Ontology declarations.", graph));
         }
+        #endregion
+
+        #region OntologyResource extensions
+        public static bool IsNamed(this OntologyResource ontResource)
+        {
+            return ontResource.Resource.IsUri();
+        }
+
+        public static Uri GetIri(this OntologyResource resource)
+        {
+            if (!resource.IsNamed())
+            {
+                throw new RdfException(string.Format("Ontology resource {0} does not have an IRI.", resource));
+            }
+
+            return ((UriNode)resource.Resource).Uri;
+        }
+
+        // TODO this method and the one above seem to overlap; simplify if possible
+        public static string GetLocalName(this OntologyResource resource)
+        {
+            if (resource.IsNamed())
+            {
+                return ((UriNode)resource.Resource).GetLocalName();
+            }
+            throw new RdfException(string.Format("{0} is not backed by a named URI node.", resource));
+        }
+
+        public static IEnumerable<INode> GetNodesViaProperty(this OntologyResource resource, INode property)
+        {
+            return resource.Graph.GetTriplesWithSubjectPredicate(resource.Resource, property).Select(triple => triple.Object);
+        }
+
+        public static bool IsDeprecated(this OntologyResource resource)
+        {
+            IUriNode deprecated = resource.Graph.CreateUriNode(new Uri("http://www.w3.org/2002/07/owl#deprecated"));
+            return resource.GetNodesViaProperty(deprecated).LiteralNodes().Any(node => node.Value == "true");
+        }
+
+        public static OntologyGraph OntologyGraph(this OntologyResource resource)
+        {
+            // TODO: Check if this is potentially explosive design
+            return resource.Graph as OntologyGraph;
+        }
+        #endregion
+
+        #region Ontology extensions
+        public static bool HasVersionIri(this Ontology ontology)
+        {
+            IUriNode versionIri = ontology.Graph.CreateUriNode(VocabularyHelper.OWL.versionIRI);
+            return ontology.GetNodesViaProperty(versionIri).UriNodes().Any();
+        }
+
+        public static Uri GetVersionIri(this Ontology ontology)
+        {
+            if (!ontology.HasVersionIri())
+            {
+                throw new RdfException(string.Format("Ontology {0} does not have an owl:versionIRI annotation", ontology));
+            }
+
+            IUriNode versionIri = ontology.Graph.CreateUriNode(VocabularyHelper.OWL.versionIRI);
+            return ontology.GetNodesViaProperty(versionIri).UriNodes().First().Uri;
+        }
+
+        /// <summary>
+        /// Gets the version IRI of the ontology, if it is defined, or the ontology
+        /// IRI if it is not. If neither is defined (i.e. the ontology is anonymous),
+        /// throws an exception.
+        /// </summary>
+        /// <param name="ontology"></param>
+        /// <returns></returns>
+        public static Uri GetVersionOrOntologyIri(this Ontology ontology)
+        {
+            if (ontology.HasVersionIri())
+            {
+                return ontology.GetVersionIri();
+            }
+            return ontology.GetIri();
+        }
 
         /// <summary>
         /// Gets a short name representation for an ontology, based on the last segment
@@ -337,5 +279,77 @@ namespace OWL2OAS
 
             return ontologyUriString;
         }
+        #endregion
+
+        #region OntologyClass extensions
+        public static bool IsRdfsDatatype(this OntologyClass oClass)
+        {
+            return oClass.Types.UriNodes().Any(classType => classType.Uri.Equals(VocabularyHelper.RDFS.Datatype));
+        }
+
+        public static bool IsRestriction(this OntologyClass oClass)
+        {
+            return oClass.Types.UriNodes().Any(classType => classType.Uri.Equals(VocabularyHelper.OWL.Restriction));
+        }
+
+        public static bool IsXsdDatatype(this OntologyClass oClass)
+        {
+            if (oClass.IsNamed())
+            {
+                return oClass.GetIri().ToString().StartsWith(XmlSpecsHelper.NamespaceXmlSchema, StringComparison.Ordinal);
+            }
+            return false;
+        }
+
+        public static IEnumerable<OntologyProperty> IsScopedDomainOf(this OntologyClass cls)
+        {
+            OntologyGraph graph = cls.Graph as OntologyGraph;
+            IUriNode onProperty = graph.CreateUriNode(new Uri("http://www.w3.org/2002/07/owl#onProperty"));
+            IEnumerable<IUriNode> propertyNodes = cls.SuperClasses.Where(superClass => superClass.IsRestriction())
+                .SelectMany(restriction => restriction.GetNodesViaProperty(onProperty)).UriNodes();
+            return propertyNodes.SelectMany(node => graph.OwlProperties.Where(oProperty => oProperty.Resource.Equals(node)));
+        }
+
+        public static IEnumerable<OntologyProperty> IsExhaustiveDomainOf(this OntologyClass oClass)
+        {
+            IEnumerable<OntologyProperty> directDomainProperties = oClass.IsDomainOf;
+            IEnumerable<OntologyProperty> indirectDomainProperties = oClass.SuperClasses.SelectMany(cls => cls.IsDomainOf);
+            IEnumerable<OntologyProperty> scopedDomainProperties = oClass.IsScopedDomainOf();
+            IEnumerable<OntologyProperty> allDomainProperties = directDomainProperties.Union(indirectDomainProperties).Union(scopedDomainProperties);
+            return allDomainProperties.Distinct(new OntologyResourceComparer()).Select(ontResource => ontResource as OntologyProperty);
+        }
+        #endregion
+
+        #region OntologyProperty extensions
+        public static bool IsObjectProperty(this OntologyProperty property)
+        {
+            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlObjectProperty));
+        }
+
+        public static bool IsDataProperty(this OntologyProperty property)
+        {
+            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlDatatypeProperty));
+        }
+
+        public static bool IsAnnotationProperty(this OntologyProperty property)
+        {
+            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.ToString().Equals(OntologyHelper.OwlAnnotationProperty));
+        }
+
+        public static bool IsFunctional(this OntologyProperty property)
+        {
+            return property.Types.UriNodes().Any(propertyType => propertyType.Uri.Equals(VocabularyHelper.OWL.FunctionalProperty));
+        }
+
+        public static IEnumerable<OntologyProperty> DataProperties(this IEnumerable<OntologyProperty> properties)
+        {
+            return properties.Where(property => property.IsDataProperty());
+        }
+
+        public static IEnumerable<OntologyProperty> ObjectProperties(this IEnumerable<OntologyProperty> properties)
+        {
+            return properties.Where(property => property.IsObjectProperty());
+        }
+        #endregion
     }
 }
